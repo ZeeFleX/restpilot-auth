4cac36a579e5798c9f75bb5d777cc14a11dccf70
Ilya Kardapolov <kardapolov@gmail.com>
2024-12-10 14:08:40 +0500

    Remove unused test methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,22 +5,4 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @RabbitRPC({
-    exchange: 'auth-exchange',
-    routingKey: 'auth.test',
-    queue: 'auth.test'
-  })
-  async testHandler(data: any) {
-    return this.appService.test(data);
-  }
-
-  @RabbitRPC({
-    exchange: 'auth-exchange',
-    routingKey: 'auth.test2',
-    queue: 'auth.test2'
-  })
-  async test2Handler(data: any) {
-    return this.appService.test2(data);
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,13 +4,4 @@ import { PrismaService } from './database/prisma.service';
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
-
-  test(data): any {
-    return data;
-  }
-
-  test2(data): any {
-    return 'Тест второй';
-  }
 }
-
